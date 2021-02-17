a989aded3fa2d5692eff567d0802b5a91d385aa6
TheDoctorWhom <Zak.dutton@gmail.com>
2021-02-17 12:58:02 -0700

    update
    added styling to app, and added loading search term on load.

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,10 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component {
 state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search',{
       params:{
@@ -15,7 +19,10 @@ state = { videos: [], selectedVideo: null };
       }
     })
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = (video) => {
@@ -27,8 +34,16 @@ state = { videos: [], selectedVideo: null };
     return (
         <div className="ui container">
           <SearchBar onFormSubmit={this.onTermSubmit}/>
-          <VideoDetail video={this.state.selectedVideo}/>
-          <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+          <div className="ui grid">
+            <div className="ui row">
+              <div className="eleven wide column">
+                <VideoDetail video={this.state.selectedVideo}/>
+              </div>
+              <div className="five wide column">
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+              </div>
+            </div>
+          </div>
          </div>
       );
   }
