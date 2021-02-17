8ba8a8dfaae0f7141631f5698b89f8393cc5f6d6
TheDoctorWhom <Zak.dutton@gmail.com>
2021-02-17 10:27:14 -0700

    updates

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,7 @@
 
   const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map((video) => {
-      return <VideoItem  onVideoSelect={onVideo Select} video= {video}/>;
+      return <VideoItem  onVideoSelect={onVideoSelect} video= {video}/>;
     });
     return <div className="ui relaxed divided list">{ renderedList }</div>
   };
